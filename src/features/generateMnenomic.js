e14443f20ca303c483523c293e3430144b1b1fdf
dennis.lee <dennis.lee@okg.com>
2023-10-16 12:04:57 +0800

    chore: minor ui enhancements

--- src/features/generateMnenomic.js
+++ src/features/generateMnenomic.js
@@ -101,16 +101,22 @@ const GenerateMnenomicCard = () => {
         </CardContent>
         <Divider flexItem />
         <CardContent sx={{ pb: 0 }}>
-          <Typography sx={{ fontSize: 20 }}>Mnenomic</Typography>
+          <Typography sx={{ fontSize: 20, pb: 1 }}>Mnenomic</Typography>
         </CardContent>
         <CardContent
-          sx={{ py: 1, mx: 2, borderRadius: 2, backgroundColor: "#f7f7f7" }}
+          sx={{
+            pt: 1,
+            pb: 2,
+            mx: 2,
+            borderRadius: 2,
+            backgroundColor: "#f7f7f7",
+          }}
         >
           <Typography sx={{ minHeight: 24, fontSize: 20 }}>
             {mnenomic}
           </Typography>
         </CardContent>
-        <CardActions sx={{ pl: 2, pr: 2, pb: 2 }}>
+        <CardActions sx={{ p: 2 }}>
           <Button
             size="small"
             variant="contained"
@@ -121,59 +127,63 @@ const GenerateMnenomicCard = () => {
             Generate Mnenomic
           </Button>
         </CardActions>
-        <CardContent sx={{ pb: 0 }}>
-          <Typography sx={{ fontSize: 20 }}>Coin Type</Typography>
-        </CardContent>
-        <CardActions sx={{ pl: 2, pr: 2, pb: 2 }}>
-          <Autocomplete
-            options={coinTypeOptions}
-            sx={{ width: 288, p: 1 }}
-            renderInput={(params) => (
-              <TextField {...params} label="Coin Type" />
+        {!!mnenomic && (
+          <>
+            <CardContent sx={{ pb: 0 }}>
+              <Typography sx={{ fontSize: 20 }}>Coin Type</Typography>
+            </CardContent>
+            <CardActions sx={{ pl: 2, pr: 2, pb: 2 }}>
+              <Autocomplete
+                options={coinTypeOptions}
+                sx={{ width: 288, p: 1 }}
+                renderInput={(params) => (
+                  <TextField {...params} label="Coin Type" />
+                )}
+                onChange={(_, value) => setCoinType(value?.value)}
+                isOptionEqualToValue={(option, value) =>
+                  option.value === value.value
+                }
+                key={!isInit}
+                disabled={!isInit}
+              />
+              <Button
+                size="small"
+                variant="contained"
+                sx={{ backgroundColor: "black", borderRadius: 2 }}
+                onClick={generatePrivateKey}
+                disabled={!isInit || !mnenomic || !coinType}
+              >
+                Generate Address
+              </Button>
+            </CardActions>
+            {errorMessage && (
+              <Alert severity="error">
+                <AlertTitle>Failure</AlertTitle>
+                {errorMessage}
+              </Alert>
             )}
-            onChange={(_, value) => setCoinType(value?.value)}
-            isOptionEqualToValue={(option, value) =>
-              option.value === value.value
-            }
-            key={!isInit}
-            disabled={!isInit}
-          />
-          <Button
-            size="small"
-            variant="contained"
-            sx={{ backgroundColor: "black", borderRadius: 2 }}
-            onClick={generatePrivateKey}
-            disabled={!isInit || !mnenomic || !coinType}
-          >
-            Generate Address
-          </Button>
-        </CardActions>
-        {errorMessage && (
-          <Alert severity="error">
-            <AlertTitle>Failure</AlertTitle>
-            {errorMessage}
-          </Alert>
+            {walletInfos &&
+              walletInfos.map((walletInfo, index) => {
+                return walletInfo ? (
+                  <>
+                    <Alert severity="success" key={index}>
+                      <AlertTitle>Success</AlertTitle>
+                      <strong>{`Chain: ${walletInfo.coinType}`}</strong>
+                      <br />
+                      <strong>{`Derivation Path: ${walletInfo.derivedPath}`}</strong>
+                      <br />
+                      <strong>{`Private Key: ${walletInfo.privateKey}`}</strong>
+                      <br />
+                      <strong>{`Address: ${walletInfo.address}`}</strong>
+                      <br />
+                      <strong>{`Public Key: ${walletInfo.publicKey}`}</strong>
+                    </Alert>
+                    <Divider flexItem key="divider" />
+                  </>
+                ) : null;
+              })}
+          </>
         )}
-        {walletInfos &&
-          walletInfos.map((walletInfo, index) => {
-            return walletInfo ? (
-              <>
-                <Alert severity="success" key={index}>
-                  <AlertTitle>Success</AlertTitle>
-                  <strong>{`Chain: ${walletInfo.coinType}`}</strong>
-                  <br />
-                  <strong>{`Derivation Path: ${walletInfo.derivedPath}`}</strong>
-                  <br />
-                  <strong>{`Private Key: ${walletInfo.privateKey}`}</strong>
-                  <br />
-                  <strong>{`Address: ${walletInfo.address}`}</strong>
-                  <br />
-                  <strong>{`Public Key: ${walletInfo.publicKey}`}</strong>
-                </Alert>
-                <Divider flexItem key="divider" />
-              </>
-            ) : null;
-          })}
       </Card>
       <Dialog
         open={showDialog}
