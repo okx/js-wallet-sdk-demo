cd9a7da98eba8cf0071ab2d4def6e655e2ae60cd
dennis.lee <dennis.lee@okg.com>
2023-10-17 11:38:29 +0800

    feat: update tests with useStore mockReturnValue

--- src/features/generateMnenomic.test.js
+++ src/features/generateMnenomic.test.js
@@ -3,10 +3,30 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 
+import { useStore } from "../stores";
 import GenerateMnenomicCard from "./generateMnenomic";
+
+jest.mock("../stores");
 
 describe("generateMnenomic", () => {
   it("should render the component", () => {
+    useStore.mockReturnValue({
+      walletStore: {
+        isInit: true,
+        coinTypeMapping: [
+          {
+            network: "BTC",
+            label: "BTC",
+            token: "BTC",
+          },
+          {
+            network: "ETH",
+            label: "ETH",
+            token: "ETH",
+          },
+        ],
+      },
+    });
     const { asFragment } = render(<GenerateMnenomicCard />);
 
     expect(asFragment()).toMatchSnapshot();
--- src/features/generatePrivateKey.test.js
+++ src/features/generatePrivateKey.test.js
@@ -3,10 +3,30 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 
+import { useStore } from "../stores";
 import GeneratePrivateKeyCard from "./generatePrivateKey";
+
+jest.mock("../stores");
 
 describe("generatePrivateKey", () => {
   it("should render the component", () => {
+    useStore.mockReturnValue({
+      walletStore: {
+        isInit: true,
+        coinTypeMapping: [
+          {
+            network: "BTC",
+            label: "BTC",
+            token: "BTC",
+          },
+          {
+            network: "ETH",
+            label: "ETH",
+            token: "ETH",
+          },
+        ],
+      },
+    });
     const { asFragment } = render(<GeneratePrivateKeyCard />);
 
     expect(asFragment()).toMatchSnapshot();
